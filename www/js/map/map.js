89205bc7cf84f9e320a61b5214ee1ef13a9ceeca
Ray Ma <rhm262@gmail.com>
2014-06-28 13:23:41 -0700

    Printing out my current location

--- www/js/map/map.js
+++ www/js/map/map.js
@@ -1,13 +1,16 @@
 angular.module('map', [])
-    .controller('MapCtrl', function() {
+    .controller('MapCtrl', function($scope) {
         var Ctrl = this;
         Ctrl.test = "hello world";
 
-//        navigator.geolocation.getCurrentPosition(function (pos) {
-//            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-//            $scope.loading.hide();
-//            $scope.myLocation = pos;
-//        }, function (error) {
-//            alert('Unable to get location: ' + error.message);
-//        });
+		navigator.geolocation.getCurrentPosition(function (pos) {
+		// Ctrl.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+		// Ctrl.loading.hide();
+			$scope.$apply(function() {
+				Ctrl.myLocation = pos;
+				console.log('my location is', pos);
+			});
+		}, function (error) {
+			alert('Unable to get location: ' + error.message);
+		});
     });
