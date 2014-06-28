47eb4ad6713fc05d7b42362a29dd2f43d19fbf4e
Alex Taylor <alexmckenley@gmail.com>
2014-06-28 12:26:25 -0700

    Updated gulp to concat all js files

--- www/js/map/map.js
+++ www/js/map/map.js
@@ -3,11 +3,11 @@ angular.module('map', [])
         var Ctrl = this;
         Ctrl.test = "hello world";
 
-        navigator.geolocation.getCurrentPosition(function (pos) {
-            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            $scope.loading.hide();
-            $scope.myLocation = pos;
-        }, function (error) {
-            alert('Unable to get location: ' + error.message);
-        });
+//        navigator.geolocation.getCurrentPosition(function (pos) {
+//            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+//            $scope.loading.hide();
+//            $scope.myLocation = pos;
+//        }, function (error) {
+//            alert('Unable to get location: ' + error.message);
+//        });
     });
