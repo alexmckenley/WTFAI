d5db10b7608aa50926de327e70018400db0101ff
Ray Ma <rhm262@gmail.com>
2014-06-28 11:51:50 -0700

    Added main state and geolocation

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,47 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', [
+  'ionic'
+])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+  .state('main', {
+    url: "/main",
+    abstract: true,
+    templateUrl: "templates/main.html"
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/main');
+})
+
+.controller('main', function () {
+  navigator.geolocation.getCurrentPosition(function (pos) {
+    $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+    $scope.loading.hide();
+    $scope.myLocation = pos;
+  }, function (error) {
+    alert('Unable to get location: ' + error.message);
+  });
+});
